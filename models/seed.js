d74e1b42bdd59c9ede3fbeba07782adeac01bd28
Jonathan Marrero <Jonathanmarrero24@gmail.com>
2022-12-13 18:37:22 -0500

    final commit

--- models/seed.js
+++ models/seed.js
@@ -8,61 +8,37 @@ mongoose.connection.on('open', () => {
      // define data we want to put in the database 
     const startingAthletes = [
         {
-            name: string,
-            fortyYardDash: number, 
-            benchPress: number, 
-            verticalJump: number, 
-            shuttleRun: number,
-            hallOfFame: Boolean
+            name: "Cam Akers",
+            fortyYardDash: 4.47, 
+            benchPress: 20, 
+            verticalJump: 35.5, 
+            shuttleRun: 4.42,
+            hallOfFame: false
         },
         {
-            name: string,
-            fortyYardDash: number, 
-            benchPress: number, 
-            verticalJump: number, 
-            shuttleRun: number,
-            hallOfFame: Boolean
+            name: "Lamar Jackson",
+            fortyYardDash: 4.58, 
+            benchPress: 10, 
+            verticalJump: 36.5, 
+            shuttleRun: 0,
+            hallOfFame: false
         },
         {
-            name: string,
-            fortyYardDash: number, 
-            benchPress: number, 
-            verticalJump: number, 
-            shuttleRun: number,
-            hallOfFame: Boolean
+            name: "Christian McCaffrey",
+            fortyYardDash: 4.48, 
+            benchPress: 10, 
+            verticalJump: 37.5, 
+            shuttleRun: 4.22,
+            hallOfFame: false
         },
         {
-            name: string,
-            fortyYardDash: number, 
-            benchPress: number, 
-            verticalJump: number, 
-            shuttleRun: number,
-            hallOfFame: Boolean
+            name: "Champ Bailey",
+            fortyYardDash: 4.28, 
+            benchPress: 0, 
+            verticalJump: 37, 
+            shuttleRun: 3.79,
+            hallOfFame: true
         },
-        {
-            name: string,
-            fortyYardDash: number, 
-            benchPress: number, 
-            verticalJump: number, 
-            shuttleRun: number,
-            hallOfFame: Boolean
-        },
-        {
-            name: string,
-            fortyYardDash: number, 
-            benchPress: number, 
-            verticalJump: number, 
-            shuttleRun: number,
-            hallOfFame: Boolean
-        },
-        {
-            name: string,
-            fortyYardDash: number, 
-            benchPress: number, 
-            verticalJump: number, 
-            shuttleRun: number,
-            hallOfFame: Boolean
-        }
     ]
 
     //delete all athletes 
